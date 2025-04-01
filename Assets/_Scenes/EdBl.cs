5ef067f479add2b354360218202a25bc10acfb3a
r618 <>
2025-04-01 20:59:43 +0200

    ass0ass

---
+++ Assets/_Scenes/EdBl.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+public class EdBl : MonoBehaviour
+{
+    [SerializeField] Camera extendedCam;
+    /*
+    void Awake()
+    {
+        Debug.Log("connected displays: " + Display.displays.Length);
+        // Display.displays[0] is the primary, default display and is always ON, so start at index 1.
+        // Check if additional displays are available and activate each.
+        for (int i = 1; i < Display.displays.Length; i++)
+        {
+            Debug.LogFormat("Display {0} activating..", i);
+            Display.displays[i].Activate();
+        }
+
+        Screen.SetResolution(Display.displays[0].systemWidth * Display.displays.Length, Display.displays[0].systemHeight, FullScreenMode.FullScreenWindow);
+    }
+    */
+
+    bool gui = false;
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F9))
+            this.gui = !this.gui;
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+            Application.Quit();
+    }
+
+    void OnGUI()
+    {
+        if (!this.gui)
+            return;
+
+        GUI.Box(new Rect(0, 0, Screen.width, Screen.height), "");
+
+        for (int i = 0; i < Display.displays.Length; i++)
+        {
+            GUILayout.Label(string.Format("#{0} {1}x{2} [rendering at {3}x{4}]", i, Display.displays[i].systemWidth, Display.displays[i].systemHeight, Display.displays[i].renderingWidth, Display.displays[i].renderingHeight));
+        }
+    }
+}
