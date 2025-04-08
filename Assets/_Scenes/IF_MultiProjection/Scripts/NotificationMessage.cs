970b4b3c59e37c83c028fa486ed261621cd8eb88
r618 <>
2025-04-08 09:16:13 +0200

    newASS cp

---
+++ Assets/_Scenes/IF_MultiProjection/Scripts/NotificationMessage.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace InteractiveFloorProjection
+{
+    [RequireComponent(typeof(Animator))]
+    public class NotificationMessage : MonoBehaviour
+    {
+        public Text messageText;
+        public float appearDuration;
+        float timer;
+        Animator animator;
+
+        void Start()
+        {
+            animator = GetComponent<Animator>();
+        }
+
+        public void Show()
+        {
+            timer = 0f;
+            animator.SetBool("visible", true);
+        }
+        public void Hide()
+        {
+            animator.SetBool("visible", false);
+            timer = 0f;
+        }
+        void Update()
+        {
+            if (animator.GetBool("visible"))
+            {
+                timer += Time.deltaTime;
+                if (timer >= appearDuration)
+                {
+                    Hide();
+                }
+            }
+        }
+    }
+}
